003a4b11c35179be4a8150f7dcbaa7b287465378
Abdurohman <su1600439@su.edu.et>
2026-08-15 08:35:33 +0300

    Add Serializer Validation and Authentication permisions

--- todos/serializers.py
+++ todos/serializers.py
@@ -3,4 +3,35 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = ['id','title','completed','created_at']
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Titleህ ቢያንስ 3 ፊደላት መሆን አለበት!")
+        return value
+    def validate(self, data):
+        title = data.get('title','')
+        completed = data.get('completed',False)
+
+        if title.lower() == 'test' and completed:
+            raise serializers.ValidationError("የ 'test' Title ያለው Todo ተጠናቋል (complated) ተብሎ ሊመዘገብ አይችልም! ")
+        return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Todo
 from .serializers import TodoSerializer
 
 class TodoListCreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
 
 class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
